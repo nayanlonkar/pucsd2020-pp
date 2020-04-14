091b69e12ff826e78939cca26ec3e290e1c8938c
Nayan Lonkar <nayanlonkar@gmail.com>
2020-04-14 15:09:50 +0530

    header and menu bar is added

--- challenges/challenge_3/challenge_3b/frontend/src/app/app-routing.module.ts
+++ challenges/challenge_3/challenge_3b/frontend/src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { CreateComponent } from './components/create/create.component';
+import { UpdateComponent } from './components/update/update.component';
+import { DeleteComponent } from './components/delete/delete.component';
+import { componentFactoryName } from '@angular/compiler';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'update', component: UpdateComponent },
+  { path: 'delete', component: DeleteComponent },
+];
+
+// const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- challenges/challenge_3/challenge_3b/frontend/src/app/app.module.ts
+++ challenges/challenge_3/challenge_3b/frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { CreateComponent } from './components/create/create.component';
+import { UpdateComponent } from './components/update/update.component';
+import { DeleteComponent } from './components/delete/delete.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { MenuComponent } from './components/layout/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    CreateComponent,
+    UpdateComponent,
+    DeleteComponent,
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
