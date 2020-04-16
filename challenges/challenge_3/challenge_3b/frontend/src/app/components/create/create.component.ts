3e1e52a9a3fbe36f1f0ebf51a4034c7e7d989554
Nayan Lonkar <nayanlonkar@gmail.com>
2020-04-16 17:04:09 +0530

    updated create

--- challenges/challenge_3/challenge_3b/frontend/src/app/components/create/create.component.ts
+++ challenges/challenge_3/challenge_3b/frontend/src/app/components/create/create.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
 })
 export class CreateComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  // private URL = 'https://jsonplaceholder.typicode.com/posts/';
+  // private URL = 'http://localhost:9090/webapi/v1/user';
+  private URL = '/api/webapi/v1/user';
+  public result;
+  public data = {
+    first_name: null,
+    last_name: null,
+    email: null,
+    contact_number: null,
+    password: null,
+    updated_by: 0,
+  };
 
-  ngOnInit(): void {
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    }),
+  };
+
+  ngOnInit(): void {}
+
+  async onSubmitCreate(values) {
+    console.log(values);
+    this.data.first_name = values.first_name;
+    this.data.last_name = values.last_name;
+    this.data.email = values.email_id;
+    this.data.contact_number = values.contact_number;
+    this.data.password = values.password;
+
+    this.result = await this.http
+      .post(this.URL, JSON.stringify(this.data), this.httpOptions)
+      .toPromise();
   }
-
 }
--- challenges/challenge_3/challenge_3b/frontend/src/app/components/update/update.component.ts
+++ challenges/challenge_3/challenge_3b/frontend/src/app/components/update/update.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-update',
   templateUrl: './update.component.html',
-  styleUrls: ['./update.component.css']
+  styleUrls: ['./update.component.css'],
 })
 export class UpdateComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  // private URL = 'https://jsonplaceholder.typicode.com/posts/';
+  // private URL = 'http://localhost:9090/webapi/v1/user';
+  private URL = '/api/webapi/v1/user';
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onSubmitUpdate(values) {}
 }
