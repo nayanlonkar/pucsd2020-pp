89622970fef1fa6d0e6dbf9d90e2e66fabc59896
Nayan Lonkar <nayanlonkar@gmail.com>
2020-04-16 16:34:26 +0530

    updated delete

--- challenges/challenge_3/challenge_3b/frontend/src/app/components/delete/delete.component.ts
+++ challenges/challenge_3/challenge_3b/frontend/src/app/components/delete/delete.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-delete',
   templateUrl: './delete.component.html',
-  styleUrls: ['./delete.component.css']
+  styleUrls: ['./delete.component.css'],
 })
 export class DeleteComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  // private URL = 'https://jsonplaceholder.typicode.com/posts/';
+  // private URL = 'http://localhost:9090/webapi/v1/user';
+  private URL = '/api/webapi/v1/user/';
 
-  ngOnInit(): void {
+  public result;
+
+  ngOnInit(): void {}
+
+  async onClickDelete(value) {
+    console.log(value);
+    this.result = await this.http.delete(this.URL + value).toPromise();
   }
-
 }
